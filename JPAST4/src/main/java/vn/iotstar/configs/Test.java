0319fcca3c10a5b51cb8124fd765ae7c168ec022
Văn Thắng <nvthangforwork@gmail.com>
2024-10-08 21:04:05 +0700

    jpa

---
+++ JPAST4/src/main/java/vn/iotstar/configs/Test.java
@@ -0,0 +1,22 @@
+package vn.iotstar.configs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class Test {
+	public static void main(String[] args) {
+		EntityManager enma = JPAConfig.getEntityManager();
+		EntityTransaction trans = enma.getTransaction();
+		try {
+			trans.begin();
+			//gọi phương thức crud
+			trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+			throw e;
+		}finally {
+			enma.close();
+		}
+	}
+}
